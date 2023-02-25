c5bb83cb8c65109c89ac997a1820269ac8b815e9
nastassya-sav <savasya08@gmail.com>
2023-02-25 23:07:06 +0300

    update js file: get elements from html

---
+++ script.js
@@ -0,0 +1,3 @@
+let timerInput = document.getElementById("time"); // Берём строку
+let buttonRun = document.getElementById("button");// Берём кнопку запуска
+let timerShow = document.getElementById("timer"); // Берём блок для показа времени
